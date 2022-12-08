17cfc34a72919648f332beaff73a6c4443ec5ded
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2022-12-08 10:50:57 +0100

    Day 2 solution

--- 2022/2/code.py
+++ 2022/2/code.py
@@ -2,13 +2,43 @@
 # Author = ?
 # Date = December 2022
 
-with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
+with open((__file__.rstrip("code.py") + "input.txt"), 'r') as input_file:
     input = input_file.read()
 
+points = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
+respective = {'A': 'X', 'B': 'Y', 'C': 'Z'}
+wins_against = {'A': 'Z', 'B': 'X', 'C': 'Y'}
+lose_against = {'A': 'Y', 'B': 'Z', 'C': 'A'}
+total = 0
+total2 = 0
+hands = input.split('\n')
+for h in hands:
+    a = h.split(" ")[0]
+    b = h.split(" ")[1]
+    # Draw scenario
+    if points[b] == points[a]:
+        total += 3 + points[b]
+    # Opponent wins
+    elif b == wins_against[a]:
+        total += points[b]
+    # I win
+    else:
+        total += 6 + points[b]
 
+for h in hands:
+    a = h.split(" ")[0]
+    b = h.split(" ")[1]
+    match b:
+        case 'X':
+            my_hand = wins_against[a]
+            total2 += points[my_hand]
+        case 'Y':
+            my_hand = respective[a]
+            total2 += 3 + points[my_hand]
+        case 'Z':
+            my_hand = lose_against[a]
+            total2 += 6 + points[my_hand]
 
-print("Part One : "+ str(None))
+print("Part One : " + str(total))
 
-
-
-print("Part Two : "+ str(None))
+print("Part Two : " + str(total2))
