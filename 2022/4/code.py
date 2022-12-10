c2544c5adc8777f974fb3d9cdb740993078b893d
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2022-12-10 11:44:11 +0100

    Day 5 solution

--- 2022/4/code.py
+++ 2022/4/code.py
@@ -1,14 +1,46 @@
 # Advent of code Year 2022 Day 4 solution
 # Author = ?
 # Date = December 2022
-
-with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
-    input = input_file.read()
-
-
-
-print("Part One : "+ str(None))
+import re
+with open((__file__.rstrip("code.py") + "input.txt"), 'r') as input_file:
+    input = input_file.read().strip().split('\n')
 
 
+"""
+Day 4: Camp Cleanup
+"""
 
-print("Part Two : "+ str(None))
+import re
+
+_PATTERN = re.compile(r"(\d+)-(\d+),(\d+)-(\d+)")
+
+
+def part1(lines):
+    """
+    >>> part1(["2-4,6-8", "2-3,4-5", "5-7,7-9", "2-8,3-7", "6-6,4-6", "2-6,4-8"])
+    2
+    """
+    count = 0
+    for line in lines:
+        start1, end1, start2, end2 = map(int, re.match(_PATTERN, line).groups())
+        if start1 <= start2 and end1 >= end2 or start1 >= start2 and end1 <= end2:
+            count += 1
+    return count
+
+
+def part2(lines):
+    """
+    >>> part2(["2-4,6-8", "2-3,4-5", "5-7,7-9", "2-8,3-7", "6-6,4-6", "2-6,4-8"])
+    4
+    """
+    count = 0
+    for line in lines:
+        start1, end1, start2, end2 = map(int, re.match(_PATTERN, line).groups())
+        if start1 <= end2 and end1 >= start2:
+            count += 1
+    return count
+
+
+parts = (part1, part2)
+
+print(part1(input), part2(input))
