697a0cc846ed686757dde11c8eeea304f6561abd
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2022-12-10 14:45:38 +0100

    Day 6 solution

--- 2022/6/code.py
+++ 2022/6/code.py
@@ -2,13 +2,20 @@
 # Author = ?
 # Date = December 2022
 
-with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
+with open((__file__.rstrip("code.py") + "input.txt"), 'r') as input_file:
     input = input_file.read()
 
 
+def part1(line, size):
+    for i, _ in enumerate(line):
+        current_substring = line[i:i+size]
+        packet = set(current_substring)
+        if len(packet) == size:
+            return i + size
+    return -1
 
-print("Part One : "+ str(None))
 
+print("Part One : " + str(part1(input, 4)))
 
-
-print("Part Two : "+ str(None))
+print("Part Two : " + str(part1(input, 14)))
+print(input[3546:3560])
