7c850191e84909c8654264f95c1d11fa6bc4bd46
grazianom-tuidi <40232320+montanarograziano@users.noreply.github.com>
2023-12-04 19:08:07 +0100

    2023 Day 4 Solution

--- 2023/4/code.py
+++ 2023/4/code.py
@@ -1,14 +1,40 @@
 # Advent of code Year 2023 Day 4 solution
 # Author = montanarograziano
 # Date = December 2023
-
-with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
-    input = input_file.read()
+import re
 
 
+def part1():
+    with open((__file__.rstrip("code.py") + "input.txt"), "r") as input_file:
+        input = list(map(lambda x: x.split(":")[1].strip(), input_file.readlines()))
+        points = 0
+        for line in input:
+            winning_cards, my_cards = line.split("|")
+            win_nums = set(re.findall(r"\d+", winning_cards))
+            my_nums = set(re.findall(r"\d+", my_cards))
+            inters = win_nums.intersection(my_nums)
+            points += 2 ** (len(inters) - 1) if len(inters) > 0 else 0
 
-print("Part One : "+ str(None))
+        return points
 
 
+def part2():
+    with open((__file__.rstrip("code.py") + "input.txt"), "r") as input_file:
+        input = list(map(lambda x: x.split(":")[1].strip(), input_file.readlines()))
+        card_dict = {i: 1 for i in range(1, len(input) + 1)}
+        for i, line in enumerate(input):
+            winning_cards, my_cards = line.split("|")
+            win_nums = set(re.findall(r"\d+", winning_cards))
+            my_nums = set(re.findall(r"\d+", my_cards))
+            inters = win_nums.intersection(my_nums)
+            if inters:
+                for j in range(i + 2, i + len(inters) + 2):
+                    card_dict[j] += 1 * card_dict[i + 1]
 
-print("Part Two : "+ str(None))
+    return sum(card_dict.values())
+
+
+print("Part One : " + str(part1()))
+
+
+print("Part Two : " + str(part2()))
