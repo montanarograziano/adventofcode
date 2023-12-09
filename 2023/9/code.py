82c96f128a684af56aa17d2c6aa219f36d3c3cfe
grazianom-tuidi <40232320+montanarograziano@users.noreply.github.com>
2023-12-09 19:58:54 +0100

    2023 Day 9 solution

--- 2023/9/code.py
+++ 2023/9/code.py
@@ -2,13 +2,52 @@
 # Author = ?
 # Date = December 2018
 
-with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
-    input = input_file.read()
+
+def part2():
+    with open((__file__.rstrip("code.py") + "input.txt"), "r") as input_file:
+        input = input_file.read()
 
 
+def help1(s):
+    if sum(i != 0 for i in s) == 0:
+        return 0
+    m = []
+    for i in range(len(s) - 1):
+        m.append(s[i + 1] - s[i])
+    return s[-1] + help1(m)
 
-print("Part One : "+ str(None))
+
+def help2(s):
+    if sum(i != 0 for i in s) == 0:
+        return 0
+    m = []
+    for i in range(len(s) - 1):
+        m.append(s[i + 1] - s[i])
+    return s[0] - help2(m)
 
 
+def part1():
+    with open((__file__.rstrip("code.py") + "input.txt"), "r") as input_file:
+        l = [
+            [int(i) for i in s.split()]
+            for s in input_file.read().split("\n")
+            if s.strip()
+        ]
 
-print("Part Two : "+ str(None))
+    return sum(help1(i) for i in l)
+
+
+def part2():
+    with open((__file__.rstrip("code.py") + "input.txt"), "r") as input_file:
+        l = [
+            [int(i) for i in s.split()]
+            for s in input_file.read().split("\n")
+            if s.strip()
+        ]
+
+    return sum(help2(i) for i in l)
+
+
+print("Part One : " + str(part1()))
+
+print("Part Two : " + str(part2()))
